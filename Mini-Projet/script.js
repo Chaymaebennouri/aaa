bed39d24e6e81b997bc0568ce83ef5c48612f06d
chiama <chaymae.bennouri@gmali.com>
2020-04-20 00:46:48 +0000

    Mini-Projet

--- Mini-Projet/script.js
+++ Mini-Projet/script.js
@@ -1,126 +1,68 @@
-window.addEventListener('load', init);
+const testwrapper = document.querySelector('.Input');
+const testarea = document.querySelector('#textArea');
+const origintext = document.querySelector('.Display').innerHTML
+const resetbutton = document.querySelector('.reset');
+const thetimer = document.querySelector('.time');
 
-// Globals
+let tiime=[0,0,0,0];
+let interval;
+let timerunning = false;
 
-// Available Levels
-const levels = {
-  easy: 5,
-  medium: 3,
-  hard: 1
-};
-
-// To change level
-const currentLevel = levels.medium;
-
-let time = currentLevel;
-let score = 0;
-let isPlaying;
-
-// DOM Elements
-const wordInput = document.querySelector('#word-input');
-const currentWord = document.querySelector('#current-word');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
-const message = document.querySelector('#message');
-const seconds = document.querySelector('#seconds');
-
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
-
-// Initialize Game
-function init() {
-  // Show number of seconds in UI
-  seconds.innerHTML = currentLevel;
-  // Load word from array
-  showWord(words);
-  // Start matching on word input
-  wordInput.addEventListener('input', startMatch);
-  // Call countdown every second
-  setInterval(countdown, 1000);
-  // Check game status
-  setInterval(checkStatus, 50);
+function zero(Time){
+  if(Time<=9){
+    Time = "0"+ Time;
+  }
+  return Time;
 }
 
-// Start match
-function startMatch() {
-  if (matchWords()) {
-    isPlaying = true;
-    time = currentLevel + 1;
-    showWord(words);
-    wordInput.value = '';
-    score++;
+function timer(){
+  let currentTime=zero(tiime[0])+":"+zero(tiime[1])+":"+zero(tiime[2]);
+  thetimer.innerHTML=currentTime;
+  tiime[3]++;
+
+  tiime[0] = Math.floor((tiime[3]/100)/60);
+  tiime[1] = Math.floor((tiime[3]/100) - (tiime[0]*60));
+  tiime[2] = Math.floor(tiime[3] - (tiime[1] * 100)-(tiime[0] * 6000));
+}
+
+function restar(){
+  clearInterval(interval);
+  interval =null;
+  tiime=[0,0,0,0];
+  thetimer = false;
+  testarea.value="",
+  thetimer.innerHTML="00:00:00";
+  testwrapper.style.borderColor='green';
+}
+
+function spel(){
+  let textenterde=textarea.value;
+  let origintextmatch = origintext.substring(0,textentered.length);
+
+  if(textentered == origintext){
+    testwrapper.style.borderColor ='orange';
+    clearInterval(interval);
+  }else{
+    if(textentered == originetextmatch){
+      testwrapper.style.borderColor ='green';
+    }else{
+      testwrapper.style.borderColor='red';
+    }
   }
 
-  // If score is -1, display 0
-  if (score === -1) {
-    scoreDisplay.innerHTML = 0;
-  } else {
-    scoreDisplay.innerHTML = score;
+}
+
+
+
+function start() { 
+  let textentered = testarea.value.length;
+  if(textenteredlength === 0){
+    timerunning = true;
+    interval=setInterval(timer,10);
+
   }
 }
 
-// Match currentWord to wordInput
-function matchWords() {
-  if (wordInput.value === currentWord.innerHTML) {
-    message.innerHTML = 'Correct!!!';
-    return true;
-  } else {
-    message.innerHTML = '';
-    return false;
-  }
-}
-
-// Pick & show random word
-function showWord(words) {
-  // Generate random array index
-  const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
-  currentWord.innerHTML = words[randIndex];
-}
-
-// Countdown timer
-function countdown() {
-  // Make sure time is not run out
-  if (time > 0) {
-    // Decrement
-    time--;
-  } else if (time === 0) {
-    // Game is over
-    isPlaying = false;
-  }
-  // Show time
-  timeDisplay.innerHTML = time;
-}
-
-// Check game status
-function checkStatus() {
-  if (!isPlaying && time === 0) {
-    message.innerHTML = 'Game Over!!!';
-    score = -1;
-  }
-}
+testarea.addEventListener('keypress',start,false);
+testarea.addEventListener('keyup',spel,false);
+resetbutton.addEventListener('click', restar ,false);
