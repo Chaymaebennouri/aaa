cccd74922d2b2fbe9bb4a442c6817d6c5d11d15a
chiama <chaymae.bennouri@gmali.com>
2020-05-27 09:50:34 +0000

    modification

---
+++ miniprojet/javascript/script.js
@@ -0,0 +1,63 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var list = document.getElementById('side')
+
+function divClecked(e){
+    console.log(e.target.innerHTML)
+}
+let httpReq=new XMLHttpRequest()
+httpReq.open("GET","https://api.covid19api.com/countries",true)
+httpReq.onreadystatechange=function(){
+    if(httpReq.readyState==4 && httpReq.status==200){
+        let resp=JSON.parse(httpReq.response)
+        resp.forEach(e => {
+            let d=document.createElement("div")
+            d.setAttribute('id',e.ISO2)
+            d.innerHTML=e.Country
+            d.addEventListener("click",divClecked)
+            list.appendChild(d)
+        });
+    }
+}
+httpReq.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Green'],
+        datasets: [{
+            label: '1' ,
+            data: [12, 19, 3, 5, 2, 3],
+
+        },
+        {   label: '2' ,
+            data: [12, 19, 13, 5, 22, 3],}
+    ]    
+    },
+    options: {
+        tooltips: {
+            mode: 'index'
+        }
+    }
+});
